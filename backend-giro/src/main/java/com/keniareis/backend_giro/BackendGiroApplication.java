473c9ed324177dc3469838f3ad614ea165ed1411
keniareis <keniapubg@gmail.com>
2025-02-15 14:56:17 -0300

    feat: initialize spring boot aplication

---
+++ backend-giro/src/main/java/com/keniareis/backend_giro/BackendGiroApplication.java
@@ -0,0 +1,13 @@
+package com.keniareis.backend_giro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGiroApplication.class, args);
+	}
+
+}
---
+++ backend-giro/src/test/java/com/keniareis/backend_giro/BackendGiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keniareis.backend_giro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
